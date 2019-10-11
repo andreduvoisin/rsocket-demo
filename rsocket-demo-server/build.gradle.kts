1d97e4b0f943caa0065bae97359cf798ca643193
Andre Duvoisin <anduv13@gmail.com>
2019-10-11 00:49:34 -0500

    initial end-to-end commit

--- rsocket-demo-server/build.gradle.kts
+++ rsocket-demo-server/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-rsocket")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
+//	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
---
+++ rsocket-demo-server/src/main/kotlin/com/andreduvoisin/rsocketdemoserver/DemoController.kt
@@ -0,0 +1,19 @@
+package com.andreduvoisin.rsocketdemoserver
+
+import org.slf4j.LoggerFactory
+import org.springframework.messaging.handler.annotation.DestinationVariable
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.stereotype.Controller
+import reactor.core.publisher.Mono
+
+@Controller
+class DemoController {
+
+    private val logger = LoggerFactory.getLogger(DemoController::class.java)
+
+    @MessageMapping("user.{id}")
+    fun user(@DestinationVariable id: String): Mono<String> {
+        logger.debug("user.{id}, id={}", id)
+        return Mono.just(id)
+    }
+}
